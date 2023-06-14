13dc2a51af8ae0e8bfe8b4a491d74b423c43bc8e
niksukhorukov <niksukhorukov@rambler.ru>
2023-06-14 16:04:31 +0300

    update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Simulator'
-copyright = '2022, Alexey Vasilev, Anna Volodkevich'
-author = 'Alexey Vasilev, Anna Volodkevich'
+project = 'Sim4Rec'
+copyright = '2022, Sberbank AI Laboratory'
+author = 'Alexey Vasilev, Anna Volodkevich, Andrey Gurov, Elizaveta Stavinova, Anton Lysenko'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
