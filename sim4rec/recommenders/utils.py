4a970fd0e1df86e68bc3f657d56ce3a5ab0073e2
Veronika Ivanova <veronika.ivanova88@yandex.ru>
2024-09-11 18:30:06 +0300

    remove replay from readme and poetry

---
+++ sim4rec/recommenders/utils.py
@@ -0,0 +1,37 @@
+from pyspark.sql.types import DoubleType, IntegerType, StructField, StructType, TimestampType
+
+
+def get_schema(
+    query_column: str = "query_id",
+    item_column: str = "item_id",
+    timestamp_column: str = "timestamp",
+    rating_column: str = "rating",
+    has_timestamp: bool = True,
+    has_rating: bool = True,
+):
+    """
+    Get Spark Schema with query_id, item_id, rating, timestamp columns
+
+    :param query_column: column name with query ids
+    :param item_column: column name with item ids
+    :param timestamp_column: column name with timestamps
+    :param rating_column: column name with ratings
+    :param has_rating: flag to add rating to schema
+    :param has_timestamp: flag to add tomestamp to schema
+    """
+    base = [
+        StructField(query_column, IntegerType()),
+        StructField(item_column, IntegerType()),
+    ]
+    if has_timestamp:
+        base += [StructField(timestamp_column, TimestampType())]
+    if has_rating:
+        base += [StructField(rating_column, DoubleType())]
+    return StructType(base)
+
+REC_SCHEMA = get_schema(
+    query_column="user_idx",
+    item_column="item_idx",
+    rating_column="relevance",
+    has_timestamp=False,
+)
